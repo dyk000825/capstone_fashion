ba925bd3156c3d56f9331d7ce6eb5167c1c658cd
dyk000825 <kdy000825@naver.com>
2024-03-24 20:33:24 +0900

    Initial commit

---
+++ src/main/java/com/example/capstone_fashion/CapstoneFashionApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstone_fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capstone_fashion/CapstoneFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone_fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
